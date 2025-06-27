fcd8ec213c993b82785bd172c673d8309b607a11
Chandra Prakash Bathula <chandu.bathula16@gmail.com>
2025-06-27 02:51:47 +0000

    started DSA

---
+++ Python.py
@@ -0,0 +1,10 @@
+# 1. Create a list of 3 fruits
+# 2. Print the first fruit
+# 3. Create a dict with your name and favorite color
+# 4. Print your favorite color
+
+fruits = ['apple','banana','mango'] 
+print(fruits[0])
+chandu = {'name':'ChandraPrakashBathula','favcolor':'skyblue'} 
+print(chandu.get('favcolor')) 
+print(chandu['favcolor'])
