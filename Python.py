860ea127b41a13ca0f86d6dde6f5d2a0b11b7a5c
Chandra Prakash Bathula <chandu.bathula16@gmail.com>
2025-06-27 14:21:10 -0500

    Update Python.py

--- Python.py
+++ Python.py
@@ -8,3 +8,8 @@ print(fruits[0])
 chandu = {'name':'ChandraPrakashBathula','favcolor':'skyblue'} 
 print(chandu.get('favcolor')) 
 print(chandu['favcolor'])
+
+if 1 > 2:
+  "Good Boy"
+else:
+  "Great Boy"
